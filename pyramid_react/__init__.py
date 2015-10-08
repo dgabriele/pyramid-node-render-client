2d9aeb1dd1cfbaa6b93268563b2e3ac1be862cf0
Daniel Gabriele <d.gabri3le@gmail.com>
2015-10-07 21:47:03 -0400

    pass template name as renderer kwarg instead of to __call__
    Signed-off-by: Daniel Gabriele <d.gabri3le@gmail.com>

--- pyramid_react/__init__.py
+++ pyramid_react/__init__.py
@@ -1,16 +1,4 @@
-
-from .client import ReactClient
-
-
 def includeme(config):
-    settings = config.get_settings()
-
-    client = ReactClient.from_settings(settings)
-
-    # transform each JSX file on the path
-    client.transform_path(settings['react.path'])
-
-    def react(req):
-        return client
-
-    config.add_request_method(react, reify=True)
+    factory = 'pyramid_react.renderer.ReactRenderer'
+    template_extension = config.registry.settings['react.extension']
+    config.add_renderer(template_extension, factory=factory)
---
+++ pyramid_react/renderer.py
@@ -0,0 +1,30 @@
+import re
+import requests
+import requests_unixsocket
+
+
+class ReactRenderer(object):
+    """ Renderer that outsources HTML rendering to react-render-service, which
+        can be found here: https://github.com/dgabriele/react-render-service.
+    """
+
+    def __init__(self, info):
+        # bind can be an http or domain socket.
+        # EG: '/tmp/react.sock' or 'localhost:8080'
+        self.template = info.name.split('.')[0]
+        self.bind = info.settings.get('react.bind')
+        if not self.bind:
+            raise ValueError('react.bind must be defined in settings')
+        if re.match(r'^(.+):(\d+)$', self.bind):
+            self.session = requests.Session()
+            self.url = 'http://{}/render'.format(self.bind)
+        else:
+            socket_path = self.bind.rstrip('/').replace('/', '%2F')
+            self.session = requests_unixsocket.Session()
+            self.url = 'http+unix://{}/render'.format(socket_path)
+
+    def __call__(self, value, system):
+        return self.session.post(self.url, json={
+            'template': self.template,
+            'context': value,
+        }).json()
